98c95af2c5a38f30be2dfa0424a82ada38d0ccfd
Georgy Gnezdilov <georgy.gnezdilov@gmail.com>
2024-04-05 08:37:49 +0300

    now you can delete multiple task at the time

--- src/taskr/cli.py
+++ src/taskr/cli.py
@@ -57,19 +57,20 @@ def create_task(title: str) -> None:
 
 
 @cli.command("remove")
-@click.argument("id")
-def remove_task(id: str) -> None:
-    try:
-        response = requests.post(uri + f"/remove/{id}")
-    except requests.ConnectionError:
-        click.echo(f"could not connect to taskr daemon", file=sys.stderr)
-        sys.exit(1)
+@click.argument("ids", nargs=-1)
+def remove_task(ids: list[str]) -> None:
+    for id in ids:
+        try:
+            response = requests.post(uri + f"/remove/{id}")
+        except requests.ConnectionError:
+            click.echo(f"could not connect to taskr daemon", file=sys.stderr)
+            sys.exit(1)
 
-    if response.status_code == 404:
-        click.echo(f"could not find task with ID={id}", file=sys.stderr)
-        sys.exit(1)
+        if response.status_code == 404:
+            click.echo(f"could not find task with ID={id}", file=sys.stderr)
+            sys.exit(1)
 
-    click.echo(f"Deleted: {id}")
+        click.echo(f"Deleted: {id}")
 
 
 def main() -> None:
